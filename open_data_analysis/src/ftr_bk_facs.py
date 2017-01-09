465d14f9963bd649bbe85674317f2749f3542132
JHWu92 <jhwu92@gmail.com>
2017-01-09 17:07:36 -0500

    var name bug fixed in ftr_bk_facs.py

--- open_data_analysis/src/ftr_bk_facs.py
+++ open_data_analysis/src/ftr_bk_facs.py
@@ -11,26 +11,31 @@ def get_feature_bk_facs(path_osm_db, path_segs, path_ftr_bk_facs, bfr_crs, bfr_f
     import pandas as pd
 
     bk_facs = select_bk_facs_with_geom(path_osm_db, ot='W')
+    # vectorizing tags
     for t in tag_for_pattern:
         bk_facs[t] = bk_facs.tags.apply(lambda x: x.get(t,''))
+    # extract features by manual rules
     for ftr in ftr_name_bk_facs:
         bk_facs[ftr] = bk_facs.apply(ftr_func[ftr], axis=1)
 
+    # match osm lns to segs
     segs = gp.read_file(path_segs)
     matched = lns_polys2lns(bk_facs, segs, bfr_crs, bfr_func, init_crs=init_crs)
     matched = matched.merge(bk_facs, left_on=index_ln, right_index=True)
+    # get seg-wise features by groupby
     features = []
-    for index_seg, grp in matched.groupby(index_seg):
-        features.append(tuple([index_seg]+[tuple(x for x in grp[c].values if (not pd.isnull(x)) and not x=='') for c in ftr_name_bk_facs]))
-    features = pd.DataFrame(features,columns=[index_seg]+ftr_name_bk_facs)
+    for idx, grp in matched.groupby(index_seg):
+        features.append(tuple([idx]+[tuple(x for x in grp[c].values if (not pd.isnull(x)) and not x=='') for c in ftr_name_bk_facs]))
 
+    features = pd.DataFrame(features,columns=[index_seg]+ftr_name_bk_facs)
+    # refine seg-wise features
     both_way = lambda x: 'both' if (x==('both',) or len(x)>1) else '' if len(x)==0 else x[0]
     features['cycle_lane'] = features['cycle_lane'].apply(both_way)
     features['cycle_way'] = features['cycle_way'].apply(both_way)
     features['side_walk'] = features['side_walk'].apply(both_way)
     features['bikable'] = features['bikable'].apply(lambda x: 'yes' if 'yes' in x else '' if len(x)==0 else 'no' )
     features['is_shared'] = features['is_shared'].apply(lambda x: 1 if len(x)>0 else 0)
-
+    # save features
     features.to_csv(path_ftr_bk_facs, encoding='utf-8')
     if debug:
         print '# bk facs in osm =', bk_facs.shape[0]
