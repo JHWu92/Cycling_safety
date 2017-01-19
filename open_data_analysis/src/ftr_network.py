9f517d7b5b121de29e766d1e240a4d448c629027
JHWu92 <jhwu92@gmail.com>
2017-01-19 11:38:11 -0500

    UPDATE: finish d/ud network features

--- open_data_analysis/src/ftr_network.py
+++ open_data_analysis/src/ftr_network.py
@@ -3,8 +3,41 @@ import snap
 import pandas as pd
 import geopandas as gp
 from constants import index_seg
-from constants import ftr_name_d_btw_cntr_SgAsEd, ftr_name_ud_bridge_SgAsEd, ftr_name_ud_btw_cntr_SgAsEd
+from constants import ftr_name_d_btw_cntr_SgAsEg, ftr_name_ud_bridge_SgAsEg, ftr_name_ud_btw_cntr_SgAsEg
 from collections import defaultdict
+import datetime
+from utils import costs
+
+
+def ftr_network_SgAsNd(path_segs, path_ftr_seg_as_node, directionality_column=None):
+    from geom_helper import intxn_from_segs
+    print 'begin ftr_network_SgAsNd'
+    # ################################################### find intxn matrix from segments
+    intxn_matrix = intxn_from_segs(path_segs, directionality_column=directionality_column)
+    print 'got intxn_matrix'
+
+    # ################################################### build SNAP Network
+    edges = intxn_matrix.values
+    nodes = pd.unique(edges.flatten())
+    nodes_size = len(nodes)
+    G = snap.TNGraph.New()
+    for n in nodes:
+        G.AddNode(int(n))
+    for ein, eout in edges:
+        G.AddEdge(int(ein), int(eout))
+    UG = snap.ConvertGraph(snap.PUNGraph, G)
+    print 'node size = {}, directed edges = {}, undirected edges = {}'.format(nodes_size, len([0 for e in G.Edges()]), len([0 for e in UG.Edges()]))
+
+    # ################################################### get network features
+    features = defaultdict(lambda: defaultdict(int))
+    if directionality_column:
+        ftr_directed_SgAsNd(G, features, nodes_size)
+    ftr_undirected_SgAsNd(UG, features)
+
+    df_ftr = pd.DataFrame.from_dict(features).T
+    df_ftr.index.name = index_seg
+    df_ftr.to_csv(path_ftr_seg_as_node)
+    return df_ftr
 
 
 def ftr_network_SgAsEg(path_segs, path_ftr_seg_as_edge, directionality_column=None):
@@ -31,12 +64,12 @@ def ftr_network_SgAsEg(path_segs, path_ftr_seg_as_edge, directionality_column=No
         if f == t:
             edge2idx[(f,t)].append(sidx)
             continue
-        if drtn=='B' or drtn==2:
+        if drtn == 'B' or drtn == 2:
             edge2idx[(f,t)].append(sidx)
             edge2idx[(t,f)].append(sidx)
-        elif drtn=='FT' or drtn==0:
+        elif drtn == 'FT' or drtn == 0:
             edge2idx[(f,t)].append(sidx)
-        elif drtn=='TF' or drtn==1:
+        elif drtn == 'TF' or drtn == 1:
             edge2idx[(t,f)].append(sidx)
 
     edges = edge2idx.keys()
@@ -56,7 +89,7 @@ def ftr_network_SgAsEg(path_segs, path_ftr_seg_as_edge, directionality_column=No
     features = defaultdict(lambda: defaultdict(int))
     if directionality_column:
         ftr_directed_SgAsEg(G, features, edge2idx)
-    ftr_undirected_SgAsEd(UG,features,edge2idx)
+    ftr_undirected_SgAsEg(UG,features,edge2idx)
 
     df_ftr = pd.DataFrame.from_dict(features).T
     df_ftr.index.name = index_seg
@@ -71,10 +104,10 @@ def ftr_directed_SgAsEg(G, features, edge2idx):
     for edge in Edgesbd:
         segidxs = edge2idx[(edge.GetVal1(), edge.GetVal2())]
         for sidx in segidxs:
-            features[sidx][ftr_name_d_btw_cntr_SgAsEd] += Edgesbd[edge]
+            features[sidx][ftr_name_d_btw_cntr_SgAsEg] += Edgesbd[edge]
 
 
-def ftr_undirected_SgAsEd(UG, features, edge2idx):
+def ftr_undirected_SgAsEg(UG, features, edge2idx):
     # in ug, the directed edge is sorted
     def get_segidxs_in_ug(f, t):
         segidxs1 = edge2idx[(f,t)]
@@ -87,124 +120,111 @@ def ftr_undirected_SgAsEd(UG, features, edge2idx):
     for edge in Edgesbud:
         segidxs = get_segidxs_in_ug(edge.GetVal1(), edge.GetVal2())
         for sidx in segidxs:
-            features[sidx][ftr_name_ud_btw_cntr_SgAsEd] += Edgesbud[edge]
+            features[sidx][ftr_name_ud_btw_cntr_SgAsEg] += Edgesbud[edge]
 
     EdgeV = snap.TIntPrV()
     snap.GetEdgeBridges(UG, EdgeV)
     for edge in EdgeV:
         segidxs = get_segidxs_in_ug(edge.GetVal1(), edge.GetVal2())
         for sidx in segidxs:
-            features[sidx][ftr_name_ud_bridge_SgAsEd] += 1
+            features[sidx][ftr_name_ud_bridge_SgAsEg] += 1
 
 
-def ftr_network_SgAsNd(path_segs, path_ftr_seg_as_node, directionality_column=None, is_dird=False):
-    from geom_helper import intxn_from_segs
-    print '----segments as nodes network----'
-    intxn_matrix = intxn_from_segs(path_segs, directionality_column=directionality_column)
-    print 'got intxn_matrix'
-    edges = intxn_matrix.values
-    nodes = pd.unique(edges.flatten())
-    features_SgAsNd = ftr_segs_as_nodes(nodes, edges, is_dird)
-    print 'got ftr sgasnd'
-    df_ftr = pd.DataFrame.from_dict(features_SgAsNd).T
-    df_ftr.index.name = index_seg
-    df_ftr.to_csv(path_ftr_seg_as_node)
-
-
-def ftr_segs_as_nodes(nodes, edges, is_dird=False):
-    # TODO: modify the feature name and function name, add SgAsNd
-    # TODO: reconsider the function name, because processs of building network of SgAsNd and SgAsEg is quite different
-
-    from collections import defaultdict
-    nodes_size = len(nodes)
-    G = snap.TNGraph.New()
-    for n in nodes:
-        G.AddNode(int(n))
-    for ein, eout in edges:
-        G.AddEdge(int(ein), int(eout))
-    UG = snap.ConvertGraph(snap.PUNGraph, G)
-    print 'node size = {}, directed edges = {}, undirected edges = {}'.format(nodes_size, len([0 for e in G.Edges()]), len([0 for e in UG.Edges()]))
-    features = defaultdict(lambda: defaultdict(int))
-    if is_dird:
-        ftr_directed(G, features, nodes_size)
-        print 'got ftr directed'
-    ftr_undirected(UG, features)
-    print 'got ftr undirected'
-    return features
-
-
-def ftr_directed(G, features, nodes_size):
+def ftr_directed_SgAsNd(G, features, nodes_size):
+    from constants import (ftr_name_d_in_deg_SgAsNd, ftr_name_d_out_deg_SgAsNd, ftr_name_d_auth_score_SgAsNd,
+                           ftr_name_d_btw_cntr_SgAsNd, ftr_name_d_clo_cntr_SgAsNd, ftr_name_d_far_cntr_SgAsNd,
+                           ftr_name_d_hub_score_SgAsNd,ftr_name_d_node_ecc_SgAsNd, ftr_name_d_page_rank_SgAsNd)
+    start_time = datetime.datetime.now()
+    print 'begin ftr_directed_SgAsNd', costs(start_time)
 
     for NI in G.Nodes():
         nid = NI.GetId()
-        features[nid]['d_in_deg'] = float(NI.GetInDeg())/(nodes_size-1)
-        features[nid]['d_out_deg'] = float(NI.GetOutDeg())/(nodes_size-1)
-        features[nid]['d_node_ecc'] = snap.GetNodeEcc(G, nid, True)
-        features[nid]['d_clo_cntr'] = snap.GetClosenessCentr(G, nid, True, True)
-        features[nid]['d_far_cntr'] = snap.GetFarnessCentr(G, nid, True, True)
-    print 'a'
+        features[nid][ftr_name_d_in_deg_SgAsNd] = float(NI.GetInDeg())/(nodes_size-1)
+        features[nid][ftr_name_d_out_deg_SgAsNd] = float(NI.GetOutDeg())/(nodes_size-1)
+        features[nid][ftr_name_d_node_ecc_SgAsNd] = snap.GetNodeEcc(G, nid, True)
+        features[nid][ftr_name_d_clo_cntr_SgAsNd] = snap.GetClosenessCentr(G, nid, True, True)
+        features[nid][ftr_name_d_far_cntr_SgAsNd] = snap.GetFarnessCentr(G, nid, True, True)
+    print 'got features for nodes', costs(start_time)
+
     Nodes = snap.TIntFltH()
     Edges = snap.TIntPrFltH()
     snap.GetBetweennessCentr(G, Nodes, Edges, 1.0, True)
     for nid in Nodes:
-        features[nid]['d_btw_cntr'] = Nodes[nid]
-    print 'between'
+        features[nid][ftr_name_d_btw_cntr_SgAsNd] = Nodes[nid]
+    print 'got betweenness', costs(start_time)
+
     PRankH = snap.TIntFltH()
     snap.GetPageRank(G, PRankH)
     for nid in PRankH:
-        features[nid]['d_page_rank'] = PRankH[nid]
-    print 'page'
+        features[nid][ftr_name_d_page_rank_SgAsNd] = PRankH[nid]
+    print 'got page Rank', costs(start_time)
+
     NIdHubH = snap.TIntFltH()
     NIdAuthH = snap.TIntFltH()
     snap.GetHits(G, NIdHubH, NIdAuthH)
     for nid in NIdHubH:
-        features[nid]['d_hub_score'] = NIdHubH[nid]
+        features[nid][ftr_name_d_hub_score_SgAsNd] = NIdHubH[nid]
     for nid in NIdAuthH:
-        features[nid]['d_auth_score'] = NIdAuthH[nid]
-    print 'his'
+        features[nid][ftr_name_d_auth_score_SgAsNd] = NIdAuthH[nid]
+    print 'Got Hit score', costs(start_time)
 
-def ftr_undirected(UG, features):
+
+def ftr_undirected_SgAsNd(UG, features):
+    from constants import (ftr_name_ud_art_pt_SgAsNd, ftr_name_ud_auth_score_SgAsNd, ftr_name_ud_bridge_SgAsNd,
+                           ftr_name_ud_clo_cntr_SgAsNd, ftr_name_ud_deg_cntr_SgAsNd, ftr_name_ud_eig_cntr_SgAsNd,
+                           ftr_name_ud_far_cntr_SgAsNd, ftr_name_ud_hub_score_SgAsNd,ftr_name_ud_node_ecc_SgAsNd,
+                           ftr_name_ud_btw_cntr_SgAsNd, ftr_name_ud_page_rank_SgAsNd)
+    start_time = datetime.datetime.now()
+    print 'begin ftr_undirected_SgAsNd', costs(start_time)
+
     for NI in UG.Nodes():
         nid = NI.GetId()
-        features[nid]['ud_deg_cntr'] = snap.GetDegreeCentr(UG, nid)
-        features[nid]['ud_node_ecc'] = snap.GetNodeEcc(UG, nid, False)
-        features[nid]['ud_clo_cntr'] = snap.GetClosenessCentr(UG, nid, True, False)
-        features[nid]['ud_far_cntr'] = snap.GetFarnessCentr(UG, nid, True, False)
-    print 'nodes'
+        features[nid][ftr_name_ud_deg_cntr_SgAsNd] = snap.GetDegreeCentr(UG, nid)
+        features[nid][ftr_name_ud_node_ecc_SgAsNd] = snap.GetNodeEcc(UG, nid, False)
+        features[nid][ftr_name_ud_clo_cntr_SgAsNd] = snap.GetClosenessCentr(UG, nid, True, False)
+        features[nid][ftr_name_ud_far_cntr_SgAsNd] = snap.GetFarnessCentr(UG, nid, True, False)
+    print 'got features for nodes', costs(start_time)
+
     NIdEigenH = snap.TIntFltH()
     snap.GetEigenVectorCentr(UG, NIdEigenH)
     for nid in NIdEigenH:
-        features[nid]['ud_eig_cntr'] = NIdEigenH[nid]
-    print 'eigen'
+        features[nid][ftr_name_ud_eig_cntr_SgAsNd] = NIdEigenH[nid]
+    print 'got eigen centrality', costs(start_time)
+
     Nodes = snap.TIntFltH()
     Edges = snap.TIntPrFltH()
     snap.GetBetweennessCentr(UG, Nodes, Edges, 1.0, False)
     for nid in Nodes:
-        features[nid]['ud_btw_cntr'] = Nodes[nid]
-    print 'btw'
+        features[nid][ftr_name_ud_btw_cntr_SgAsNd] = Nodes[nid]
+    print 'got btw centrality', costs(start_time)
+
     PRankH = snap.TIntFltH()
     snap.GetPageRank(UG, PRankH)
     for nid in PRankH:
-        features[nid]['ud_page_rank'] = PRankH[nid]
-    print 'page ud'
+        features[nid][ftr_name_ud_page_rank_SgAsNd] = PRankH[nid]
+    print 'got page rank', costs(start_time)
+
     NIdHubH = snap.TIntFltH()
     NIdAuthH = snap.TIntFltH()
     snap.GetHits(UG, NIdHubH, NIdAuthH)
     for nid in NIdHubH:
-        features[nid]['ud_hub_score'] = NIdHubH[nid]
+        features[nid][ftr_name_ud_hub_score_SgAsNd] = NIdHubH[nid]
     for nid in NIdAuthH:
-        features[nid]['ud_auth_score'] = NIdAuthH[nid]
-    print 'hit ud'
+        features[nid][ftr_name_ud_auth_score_SgAsNd] = NIdAuthH[nid]
+    print 'got hit score', costs(start_time)
+
     ArtNIdV = snap.TIntV()
     snap.GetArtPoints(UG, ArtNIdV)
     for nid in ArtNIdV:
-        features[nid]['ud_art_pt']=1
-    print 'artpoint'
+        features[nid][ftr_name_ud_art_pt_SgAsNd]=1
+    print 'got articulate point', costs(start_time)
+
     EdgeV = snap.TIntPrV()
     snap.GetEdgeBridges(UG, EdgeV)
     for edge in EdgeV:
-        features[edge.GetVal1()]['ud_bridge']+=1
-        features[edge.GetVal2()]['ud_bridge']+=1
+        features[edge.GetVal1()][ftr_name_ud_bridge_SgAsNd]+=1
+        features[edge.GetVal2()][ftr_name_ud_bridge_SgAsNd]+=1
+    print 'got bridge', costs(start_time)
 
 
 # ############################## Deprecated
