1641690d34788146a0fdb2aec9a79f924627f915
JHWu92 <jhwu92@gmail.com>
2017-01-19 11:37:18 -0500

    ADD: utils <-- costs()

--- open_data_analysis/src/utils.py
+++ open_data_analysis/src/utils.py
@@ -34,3 +34,16 @@ def add_suffix(fn, suf):
     import os
     base, ext = os.path.splitext(fn)
     return base + suf + ext
+
+
+def costs(start_time):
+    import datetime
+    dnow = datetime.datetime.now()
+    delta = dnow - start_time
+    delta_total_seconds_int = int(delta.total_seconds())
+    return 'now = %s, costs = %d days %02d:%02d:%02d = %d seconds' % (dnow.strftime('%Y-%m-%d %H:%M:%S'),
+                                                                      delta_total_seconds_int / 3600 / 24,
+                                                                      delta_total_seconds_int / 3600 % 24,
+                                                                      delta_total_seconds_int / 60 % 60,
+                                                                      delta_total_seconds_int % 60,
+                                                                      delta_total_seconds_int)
