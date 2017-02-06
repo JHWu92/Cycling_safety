5aebd1433232d38436b35336d8073a426c72c5a6
JHWu92 <jhwu92@gmail.com>
2017-02-06 13:41:46 -0500

    move db config to config.inc.php 2

--- public_html/rate-video.php
+++ public_html/rate-video.php
@@ -5,19 +5,17 @@
     include_once('config.inc.php');  //$db_name, $host, $db_user, $db_pwd 
     $con=mysqli_connect($host, $db_user, $db_pwd, $db_name);
 
-	$rating = $_POST["inlineRadioOptions"];
-	$comment = $_POST["Description"];
-	$vid_id = 3;
-
-	$experience = $_POST["experienceLevel"];
-	$db_name = "cyclings_vid1";
-	$con=mysqli_connect("localhost","cyclings_jiahui","@2n54a=@]ZQ4", $db_name);
-
 	if(mysqli_connect_errno())
 	{
 		echo"failed to connect to mysql:".mysqli_connect_error();
 	}
 	else
+        	$rating = $_POST["inlineRadioOptions"];
+        	$comment = $_POST["Description"];
+        	$vid_id = 3;
+
+        	$experience = $_POST["experienceLevel"];
+
 		$table_name = "Rating";
 		$sql="INSERT INTO ".$table_name." (comment, ratingScore, userid, email, videoid) VALUES ('".$comment."', '".$rating."', '".$_SESSION["user_id"]."', '".$_SESSION["email"]."', '".$_SESSION["videoid"]."')";
 		if (mysqli_query($con,$sql))
