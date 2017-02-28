274051eddb8254d215c5d9c091ab18541e1af76e
JHWu92 <jhwu92@gmail.com>
2017-02-28 13:40:04 -0500

    Developing visualization of raw and snapped gpx

--- video_gpx/vis_gpx_snap.py
+++ video_gpx/vis_gpx_snap.py
@@ -12,4 +12,3 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='visualize raw gpx and snapped traces')
     parser.add_argument('-i', '--input-directory', type=str, help='directory of raw gpx trace')
     parser.add_argument('-s', '--snapped-directory', type=str, help='directory of snapped gpx trace', default='split')
-    
