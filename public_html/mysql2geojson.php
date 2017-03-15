ada76f8598aabfb124cec4663533cea03fc4e977
JHWu92 <jhwu92@gmail.com>
2017-03-14 20:05:17 -0400

    change wkb to wkt

--- public_html/mysql2geojson.php
+++ public_html/mysql2geojson.php
@@ -1,4 +1,6 @@
 <?php
+    error_reporting(E_ALL);
+
 /**
  * Title:   MySQL to GeoJSON (Requires https://github.com/phayes/geoPHP)
  * Notes:   Query a MySQL table or view and return the results in GeoJSON format, suitable for use in OpenLayers, Leaflet, etc.
@@ -9,8 +11,8 @@
 
 # Include required geoPHP library and define wkb_to_json function
 include_once('geoPHP/geoPHP.inc');
-function wkb_to_json($wkb) {
-    $geom = geoPHP::load($wkb,'wkb');
+function wkb_to_json($wkt) {
+    $geom = geoPHP::load($wkt,'wkt');
     return $geom->out('json');
 }
 # Connect to MySQL database
@@ -20,7 +22,7 @@ $con=mysqli_connect($host, $db_user, $db_pwd, $db_name);
 if(mysqli_connect_errno()){  die("failed to connect to mysql".mysqli_connect_error()); }
 
 # Build SQL SELECT statement and return the geometry as a WKB element
-$sql = 'SELECT *, AsWKB(geometry) AS wkb FROM RoadSegment';
+$sql = 'SELECT * FROM RoadSegment';
 # Try query or error
 $rs = $con->query($sql);
 if (!$rs) {
@@ -36,12 +38,11 @@ $geojson = array(
 # Loop through rows to build feature arrays
 while ($row = $rs->fetch_assoc()) {
     $properties = $row;
-    # Remove wkb and geometry fields from properties
-    unset($properties['wkb']);
+    # Remove geometry fields from properties
     unset($properties['geometry']);
     $feature = array(
          'type' => 'Feature',
-         'geometry' => json_decode(wkb_to_json($row['wkb'])),
+         'geometry' => json_decode(wkb_to_json($row['geometry'])),
          'properties' => $properties
     );
     # Add feature arrays to feature collection array
