c37aea6fc012144b04c388ced844ebfcce19d86a
JHWu92 <jhwu92@gmail.com>
2017-05-26 13:39:28 -0400

    get joint feature for predictor

--- open_data_analysis/src/ftr_aggregate.py
+++ open_data_analysis/src/ftr_aggregate.py
@@ -3,6 +3,29 @@ import pandas as pd
 import numpy as np
 from src.constants import fn_features_dc, dir_data, index_seg, features_for_total
 
+def load_joint_features(years=(2014,2015,2016,2017), how='NO_TOTAL'):
+    """
+    for each feature files: get dummies, dummy_na=True; fillna with 0; encode each feature names with ftr_name_No
+    return joint features and code book
+    """
+    features = load_separate_features()
+    joint_features = []
+    ftr_code2desc = {}
+    for name, df in features.items():
+        ftr = df.copy()
+        ftr = filter_year(ftr, years=years)
+        if name in features_for_total:
+            ftr = filter_total(ftr, how=how)
+        ftr,mapping = encode_col(ftr, name)
+        ftr = ftr.groupby(level=0).sum()
+
+        joint_features.append(ftr)
+        ftr_code2desc.update(mapping)
+
+    joint_features = reduce(lambda x, y: pd.merge(x, y, left_index=True, right_index=True, how='outer'), joint_features)
+    
+    return joint_features, ftr_code2desc
+
 
 def load_separate_features():
     """
