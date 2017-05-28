1e8ebb80a41bbe6e6b3a6c77abf92833cdeaff64
JHWu92 <jhwu92@gmail.com>
2017-05-27 21:01:17 -0400

    exclude LTS.isnull

--- open_data_analysis/grid_cv_pred_lts.py
+++ open_data_analysis/grid_cv_pred_lts.py
@@ -21,15 +21,15 @@ if __name__ == '__main__':
     if not os.path.exists(cv_dir):
         os.mkdir(cv_dir)
 
-    # load feature and fill NAN
+    print 'load feature and fill NAN'
     ftr, mapping = load_joint_features(years=years, how=total)
     ftr = fillna(ftr, how=fna)
 
-    # load LTS and remove 10
+    print 'load LTS and remove 10'
     lts = pd.read_csv(dir_data + fn_target_lts_dc, index_col=0)
-    lts = lts[lts.LTS != 10]
+    lts = lts[lts.LTS!=10].dropna()
 
-    # create train and test set
+    print 'create train and test set'
     dataset = lts.merge(ftr, left_index=True, right_index=True)
     train, test = train_test_split(dataset, test_size=0.2, random_state=0)
     train_y = train.LTS
@@ -37,12 +37,12 @@ if __name__ == '__main__':
     test_y = test.LTS
     test_x = test.drop('LTS', axis=1)
 
-    # normalize X
+    print 'normalize X'
     scaler = MinMaxScaler().fit(train_x)
     train_x = scaler.transform(train_x)
     test_x = scaler.transform(test_x)
 
-    # get models and grid_cv tuning parameters
+    print 'get models and grid_cv tuning parameters'
     models = sk_models()
     params = grid_cv_default_params()
 
