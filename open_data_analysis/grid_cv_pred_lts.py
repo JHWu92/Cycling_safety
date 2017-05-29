342e1865eaf338b6a56ed630639ee966bd271bf4
JHWu92 <jhwu92@gmail.com>
2017-05-29 10:24:16 -0400

    separate load lts dataset

--- open_data_analysis/grid_cv_pred_lts.py
+++ open_data_analysis/grid_cv_pred_lts.py
@@ -2,45 +2,22 @@
 
 import os
 
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-
-from src.constants import fn_target_lts_dc, dir_data
-from src.ftr_aggregate import load_joint_features
-from src.sk_ml import *
+from src.pred_lts import prepare_lts_dataset
+from src.sk_ml import scaler_by_name, sk_models, grid_cv_default_params, grid_cv_models, evaluate_grid_cv, \
+    evaluator_scalable_cls
 
 if __name__ == '__main__':
     fna = 0.0
     years = (2014, 2015, 2016, 2017)
-    total = 'TOTAL'
-    normalization = 'MinMaxScaler'
+    total_or_not = 'TOTAL'
+    scaler_type = 'MinMaxScaler'
 
-    cv_dir = 'data/cross_validation/na_{}-year_{}-total_{}-norm_{}'.format(fna, years, total, normalization)
-
+    cv_dir = 'data/cross_validation/na_{}-year_{}-total_{}-norm_{}'.format(fna, years, total_or_not, scaler_type)
     if not os.path.exists(cv_dir):
         os.mkdir(cv_dir)
 
-    print 'load feature and fill NAN'
-    ftr, mapping = load_joint_features(years=years, how=total)
-    ftr = fillna(ftr, how=fna)
-
-    print 'load LTS and remove 10'
-    lts = pd.read_csv(dir_data + fn_target_lts_dc, index_col=0)
-    lts = lts[lts.LTS!=10].dropna()
-
-    print 'create train and test set'
-    dataset = lts.merge(ftr, left_index=True, right_index=True)
-    train, test = train_test_split(dataset, test_size=0.2, random_state=0)
-    train_y = train.LTS
-    train_x = train.drop('LTS', axis=1)
-    test_y = test.LTS
-    test_x = test.drop('LTS', axis=1)
-
-    print 'normalize X'
-    scaler = MinMaxScaler().fit(train_x)
-    train_x = scaler.transform(train_x)
-    test_x = scaler.transform(test_x)
+    scaler = scaler_by_name(scaler_type)
+    train_x, train_y, test_x, test_y = prepare_lts_dataset(scaler, fna, years, total_or_not)
 
     print 'get models and grid_cv tuning parameters'
     models = sk_models(stoplist=())
