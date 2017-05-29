636d646a0da9d1ca036f0082e3c15a6230e36f78
JHWu92 <jhwu92@gmail.com>
2017-05-29 11:33:37 -0400

    UPD - return ftr_name and col2code

--- open_data_analysis/src/pred_lts.py
+++ open_data_analysis/src/pred_lts.py
@@ -6,16 +6,17 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 
-def prepare_lts_dataset(scaler, fna=0.0, years=(2014,2015,2016,2017), total_or_not='total', return_type='list'):
+def prepare_lts_dataset(scaler, fna=0.0, years=(2014, 2015, 2016, 2017), total_or_not='total', return_type='list'):
     assert return_type in ('list', 'dict'), 'allowed return type: "list" or "dict"'
 
     print 'loading feature and fill NAN'
-    ftr, mapping = load_joint_features(years=years, how=total_or_not)
+    ftr, col2code = load_joint_features(years=years, how=total_or_not)
+    ftr_name = list(ftr.columns)
     ftr = fillna(ftr, how=fna)
 
     print 'loading LTS and remove 10'
     lts = pd.read_csv(dir_data + fn_target_lts_dc, index_col=0)
-    lts = lts[lts.LTS!=10].dropna()
+    lts = lts[lts.LTS != 10].dropna()
 
     print 'creating train and test set'
     dataset = lts.merge(ftr, left_index=True, right_index=True)
@@ -30,8 +31,9 @@ def prepare_lts_dataset(scaler, fna=0.0, years=(2014,2015,2016,2017), total_or_n
     train_x = scaler.transform(train_x)
     test_x = scaler.transform(test_x)
     if return_type == 'list':
-        return train_x, train_y, test_x, test_y
+        return train_x, train_y, test_x, test_y, ftr_name, col2code
     elif return_type == 'dict':
-        return {'train_x': train_x, 'train_y': train_y, 'test_x': test_x, 'test_y': test_y}
+        return {'train_x' : train_x, 'train_y': train_y, 'test_x': test_x, 'test_y': test_y,
+                'ftr_name': ftr_name, 'col2code': col2code}
     else:
         return None
